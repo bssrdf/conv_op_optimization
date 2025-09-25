65e3652f459a2b0f4d0384e69eb67748afedc14d
bssrdf <merlintiger@hotmail.com>
2025-09-24 21:50:44 -0400

    restore initializing tensors with random data

--- cuda/implicitgemm/main.cu
+++ cuda/implicitgemm/main.cu
@@ -33,7 +33,7 @@ int main(int argc, char **argv)
     unsigned int k_split = atoi(argv[13]);
 
 
-    const int ksplit = 32;
+    
     int outh = (h - r + 2 * p) / u + 1;
     int outw = (w - s + 2 * q) / v + 1;
     double M = k;
@@ -62,19 +62,19 @@ int main(int argc, char **argv)
         input[i] = (rand() % 255) / 255.0;
     }
 
-    // for (int i = 0; i < k * c * r * s; i++)
-    // {
+    for (int i = 0; i < k * c * r * s; i++)
+    {
+        weight[i] = (rand() % 255) / 255.0;
+        // weight[i] =  i / 1000.0;
+    }
+    // for(int j= 0; j < k; j++){
+    // for(int C= 0; C < c; C++){
+    // for (int i = 0; i < r * s; i++){
     //     // weight[i] = (rand() % 255) / 255.0;
-    //     weight[i] =  i / 1000.0;
+    //     weight[j*r*s*c + C*r*s + i] = j * 10 + C/100.0 + i / 1000.0;
     // }
-    for(int j= 0; j < k; j++){
-    for(int C= 0; C < c; C++){
-    for (int i = 0; i < r * s; i++){
-        // weight[i] = (rand() % 255) / 255.0;
-        weight[j*r*s*c + C*r*s + i] = j * 10 + C/100.0 + i / 1000.0;
-    }
-    }
-    }
+    // }
+    // }
 
     // for(int j= 0; j < k; j++)
     // for(int C= 0; C < 24; C++)
@@ -156,26 +156,26 @@ int main(int argc, char **argv)
     cudaEventDestroy(start);
     cudaEventDestroy(stop);
 
-    // printf("===================start verfiy===================\n");
-    // if(param.nchw)
-    //     direct_conv2dcpu(input, weight, bias, output, n, c, h, w, k, r, s, u, v, p, q);
-    // else
-    //     direct_conv2dcpu_nhwc(input, weight, bias, output, n, c, h, w, k, r, s, u, v, p, q);
+    printf("===================start verfiy===================\n");
+    if(param.nchw)
+        direct_conv2dcpu(input, weight, bias, output, n, c, h, w, k, r, s, u, v, p, q);
+    else
+        direct_conv2dcpu_nhwc(input, weight, bias, output, n, c, h, w, k, r, s, u, v, p, q);
 
-    // int error = 0;
-    // for (int i = 0; i < n * k * outh * outw; i++)
-    // {
-    //     if(i < 100)
-    //         // printf(" postion:%d, gpuvalue:%f, cpuvalue:%f\n", i, output_host[i], output[i]);
-    //     if (abs(output_host[i] - output[i]) > getPrecision(output[i]))
-    //     {
-    //         printf("error, postion:%d, gpuvalue:%f, cpuvalue:%f\n", i, output_host[i], output[i]);
-    //         error++;
-    //         break;
-    //     }
+    int error = 0;
+    for (int i = 0; i < n * k * outh * outw; i++)
+    {
+        if(i < 100)
+            // printf(" postion:%d, gpuvalue:%f, cpuvalue:%f\n", i, output_host[i], output[i]);
+        if (abs(output_host[i] - output[i]) > getPrecision(output[i]))
+        {
+            printf("error, postion:%d, gpuvalue:%f, cpuvalue:%f\n", i, output_host[i], output[i]);
+            error++;
+            break;
+        }
             
-    // }
-    // printf("================finish,error:%d=========================\n", error);
+    }
+    printf("================finish,error:%d=========================\n", error);
 
     float timePerConv = time_elapsed / iternum;
     double gflops = flopsPerConv / (timePerConv / 1000.0f);
