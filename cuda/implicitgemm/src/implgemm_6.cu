ca5da31270fb0a16c04a5502735e63a2852d8887
bssrdf <merlintiger@hotmail.com>
2025-09-28 20:06:11 -0400

    fixed a bug when number of channels < 8

--- cuda/implicitgemm/src/implgemm_6.cu
+++ cuda/implicitgemm/src/implgemm_6.cu
@@ -67,7 +67,7 @@ __global__ void implgemm(param_t param)
     }
 // ldg
 
-    if (by * 128 + tx / 2  < param.k){
+    if (by * 128 + tx / 2  < param.k && tx % 2 * 4 < param.c * param.r * param.s){
         // int inOffsetTmp = curH * inChannelOffset + curW * param.c + curC;
         float4 tmp = reinterpret_cast<float4 *>(&param.weight[by * 128 + (tx / 2) * weightKOffset + tx % 2 * 4])[0];
         weight_ldg_reg[0] = tmp.x;
@@ -138,7 +138,7 @@ __global__ void implgemm(param_t param)
     for (int crs = 0; crs < param.r * param.s * param.c; crs += 8)
     {
         // ldg
-        if (by * 128 + tx / 2 < param.k){
+        if (by * 128 + tx / 2 < param.k && tx % 2 * 4 < param.c * param.r * param.s){
             float4 tmp = reinterpret_cast<float4 *>(&param.weight[by * 128 + tx / 2 * weightKOffset + tx % 2 * 4 + crs + 8])[0];
             weight_ldg_reg[0] = tmp.x;
             weight_ldg_reg[1] = tmp.y;
